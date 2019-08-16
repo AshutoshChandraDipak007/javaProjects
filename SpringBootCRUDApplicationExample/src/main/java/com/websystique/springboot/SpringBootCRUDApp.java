84534007ad2b9b313d8ded13770b105ad1413993
AshutoshDipak <AshutoshDipak@DESKTOP-LTEQPB7>
2019-08-16 15:45:30 +0530

    Hellonew commite from eclips

--- SpringBootCRUDApplicationExample/src/main/java/com/websystique/springboot/SpringBootCRUDApp.java
+++ SpringBootCRUDApplicationExample/src/main/java/com/websystique/springboot/SpringBootCRUDApp.java
@@ -9,6 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootCRUDApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCRUDApp.class, args);
+		SpringApplication.run(SpringBootCRUDApp.class, args);// hello new commite changes
 	}
 }
