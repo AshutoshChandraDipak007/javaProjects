203198cea52b64e050f002e421c4039d2cad1d87
AshutoshChandraDipak007 <ashutoshchandra123@gmail.com>
2019-08-18 15:50:47 +0530

    gcyf

--- SecureSpringbootAppUsingSecurityDB/src/main/java/com/telusko/secureapp/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.telusko.secureapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-	public User findByUsername(String username);
-}
