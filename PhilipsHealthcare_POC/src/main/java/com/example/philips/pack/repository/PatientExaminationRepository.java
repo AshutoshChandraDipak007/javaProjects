0e01dedabac8c6624ffc3138e6093868af508642
AshutoshChandraDipak007 <ashutoshchandra123@gmail.com>
2019-08-18 10:12:23 +0530

    last commit

--- PhilipsHealthcare_POC/src/main/java/com/example/philips/pack/repository/PatientExaminationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.philips.pack.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.philips.pack.model.PatientExamination;
-
-public interface PatientExaminationRepository extends JpaRepository<PatientExamination,Integer> {
-
-}
--- PhilipsHealthcare_POC/src/main/java/com/example/philips/pack/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.philips.pack.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.philips.pack.model.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-
-}
